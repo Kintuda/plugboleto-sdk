a151c23c068cab5cb03643913a38c0d6b7e51e51
Kin Tuda <ktuda@localhost.localdomain>
2019-11-02 17:12:36 -0300

    Remove projeto antigo

--- test/send.test.js
+++
@@ -1,4 +0,0 @@
-const Boleto = require('../lib/Boleto')
-const boleto = new Boleto('staging','cpf_cnpj','token_sh','cpf_cnpjcedente')
-
-boleto.solicitarRemessa(['HyZEXKtP_g']).then(res => console.log(res))
